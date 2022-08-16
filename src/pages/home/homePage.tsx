f6ad257f4345d08e88f02072da89e5607f65d01b
Panas18 <panas.tiwary18@gmail.com>
2022-08-16 12:14:40 +0545

    add contact form and api integration

--- src/pages/home/homePage.tsx
+++ src/pages/home/homePage.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import ContactTable from "../../components/contactTable/contactTable";
 import "./homePage.css";
+import { useLayoutEffect } from "react";
 
 const HomePage: React.FC = () => {
+  useLayoutEffect(() => {
+    document.body.style.backgroundColor = "#F2F2EE";
+  });
   return (
     <div className="table--container">
       <ContactTable />
