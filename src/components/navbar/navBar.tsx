ca8d9fe1ce27cc5a0454a2788f5050791675aa83
Panas18 <panas.tiwary18@gmail.com>
2022-08-17 17:59:59 +0545

    implements information notification

--- src/components/navbar/navBar.tsx
+++ src/components/navbar/navBar.tsx
@@ -1,8 +1,14 @@
 import { NavLink, Outlet } from "react-router-dom";
 import { Link } from "react-router-dom";
 import "./navBar.css";
+import openNotification from "../../utils/notification";
 
 export const NavBar = () => {
+  const handleLogout = () => {
+    openNotification("Logout Successful");
+    localStorage.removeItem("accessToken");
+  };
+
   const Styling = (prop: { isActive: boolean }) => {
     const { isActive } = prop;
     return { color: isActive ? "#1990FE" : "gray" };
@@ -24,7 +30,12 @@ export const NavBar = () => {
           >
             Add Contact
           </NavLink>
-          <NavLink className="logout" style={Styling} to={"/login"}>
+          <NavLink
+            onClick={handleLogout}
+            className="logout"
+            style={Styling}
+            to="/login"
+          >
             Logout
           </NavLink>
         </div>
